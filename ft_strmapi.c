b1af120c8b88107df25764ae5f78491cd5e4438d
Federico Porciello <fporciel@e3r12p7.42roma.it>
2023-02-09 18:03:53 +0100

    lol

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,20 +6,18 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 11:24:12 by fporciel          #+#    #+#             */
-/*   Updated: 2023/02/09 17:48:20 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/02/09 18:03:26 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	ft_strmchck(char const *s, char (*f)(unsigned int, char))
+static unsigned int	ft_strmchck(char const *s)
 {
 	unsigned int	i;
 
-	if (*s == 0)
-		return (0);
 	i = 0;
-	while ((s[i]) && (f(i, s[i])))
+	while (s[i])
 		i++;
 	return (i);
 }
@@ -32,7 +30,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 
 	if ((s == NULL) || (*f == NULL))
 		return (NULL);
-	slen = ft_strmchck(s, f);
+	slen = ft_strmchck(s);
 	strmapi = (char *)malloc(sizeof(char) * (slen + 1));
 	if (strmapi == NULL)
 		return (NULL);
