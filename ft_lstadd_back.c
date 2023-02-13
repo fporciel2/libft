1efffeaec45f41c4d4dadf9752cee5857921c06f
Federico Porciello <fporciel@e4r7p4.42roma.it>
2023-02-13 17:48:58 +0100

    lol

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: fporciel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:56:42 by fporciel          #+#    #+#             */
-/*   Updated: 2023/02/13 17:12:55 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/02/13 17:43:54 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if ((lst == NULL) || (new == NULL))
 		return ;
-	while ((*lst != NULL) && ((*lst)->next != NULL))
-		*lst = (*lst)->next;
-	new->next = NULL;
-	if (*lst != NULL)
-		*lst = new;
+	if (*lst)
+		ft_lstlast(*lst)->next = new;
 	else
+		*lst = new;
 }
